f704c3cac6af0cc9cbb209f16c44e584c1987a2a
Dzmitry Manko <dzmitry_manko@goodyear.com>
2018-11-25 23:59:07 +0300

    PROCEDURE "ENTERPRISE_TWIN"."TwinRoutingShortestPath"

--- twin/src/main/resources/sql/CREATE_PROCEDURE_TwinRotingShortestPath.sql
+++ twin/src/main/resources/sql/CREATE_PROCEDURE_TwinRotingShortestPath.sql
@@ -1,23 +1,21 @@
 DROP TYPE "TwinRoutingTT";
 CREATE TYPE "TwinRoutingTT" AS TABLE ("segment" BIGINT, "sourceNodeID" VARCHAR(100), "targetNodeID" VARCHAR(100));
 
-CREATE OR REPLACE PROCEDURE "TwinRoutingShortestPath" (IN startNodeID VARCHAR(100), IN targetNodeID VARCHAR(100), OUT totalSegments BigInt, OUT routing "TwinRoutingTT")
+CREATE OR REPLACE PROCEDURE "ENTERPRISE_TWIN"."TwinRoutingShortestPath" (IN startNodeID VARCHAR(100), IN targetNodeID VARCHAR(100), OUT totalSegments BigInt, OUT routing "TwinRoutingTT")
  LANGUAGE GRAPH READS SQL DATA AS
   BEGIN
 	Graph g = Graph("ENTERPRISE_TWIN", "GRAPH");
-	Vertex sourceVertex = Vertex(:g, :startNodeID);
-	Vertex targetVertex = Vertex(:g, :targetNodeID);
-	WeightedPath<BigInt> p = SHORTEST_PATH(:g, :sourceVertex, :targetVertex);
+	
+	Graph gP = Subgraph(:g, e IN Edges(:g) WHERE :e."edgeType"=='path');
+	
+	Vertex sourceVertex = Vertex(:gP, :startNodeID);
+	Vertex targetVertex = Vertex(:gP, :targetNodeID);
+	WeightedPath<BigInt> p = SHORTEST_PATH(:gP, :sourceVertex, :targetVertex);
 	totalSegments = Length(:p);
-	
-/*	nodeChain = :startNodeID;
-	FOREACH e IN Edges(:p) {
-		nodeChain = SYS.CONCAT(:nodeChain, ":" );
-		nodeChain = SYS.CONCAT(:nodeChain, :e."targetNodeID");
-		
-	} */
-	
+
 	routing = SELECT :segment, :e."sourceNodeID",  :e."targetNodeID" FOREACH e in Edges(:p) WITH ORDINALITY AS segment;
+	
+	
   END;
   
   CALL "TwinRoutingShortestPath"('ROOM-3A-104', 'ROOM-5C-114', ?, ?);
